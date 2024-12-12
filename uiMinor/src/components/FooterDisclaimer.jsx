308f3afd8a71aff08917146a4c4e7e0954e33350
theayusharma <protocolayush@gmail.com>
2024-12-12 20:09:49 +0530

    footer

---
+++ uiMinor/src/components/FooterDisclaimer.jsx
@@ -0,0 +1,14 @@
+function FooterDisclaimer(){
+    return (
+        <div>
+            <footer className="bg-gray-800 text-white text-center text-sm ">
+        <p>
+          Disclaimer: This application is not a substitute for professional medical advice. Always consult a healthcare provider for an accurate diagnosis.
+        </p>
+
+      </footer>
+      </div>
+    );
+}
+
+export default FooterDisclaimer;
---
+++ uiMinor/src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import MenuBar from './MenuBar';
+import FooterDisclaimer from './FooterDisclaimer';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <MenuBar />
+      
+      <main className="flex-grow">{children}</main>
+      <FooterDisclaimer />
+    </div>
+  );
+};
+
+export default Layout;
